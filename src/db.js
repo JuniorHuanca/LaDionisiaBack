8099d49b2e381a33490a7b2fdeb8d6a33be51c54
elvis huanca <JHuanca_21@outlook.com>
2023-03-06 11:30:53 -0500

    ssl: true

--- src/db.js
+++ src/db.js
@@ -9,6 +9,7 @@ const {
 const sequelize = new Sequelize(DBPOSTGRES, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  ssl: true
 });
 // const sequelize = new Sequelize(`postgresql://${ PGUSER }:${ PGPASSWORD }@${ PGHOST }:${ PGPORT }/${ PGDATABASE }`, {
 //   logging: false, // set to console.log to see the raw SQL queries
@@ -18,7 +19,7 @@ const sequelize = new Sequelize(DBPOSTGRES, {
 // Testing connection to database
 sequelize
   .authenticate()
-  .then(function(err) {
+  .then(function (err) {
     console.log('Connection has been established successfully.');
   })
   .catch(function (err) {
